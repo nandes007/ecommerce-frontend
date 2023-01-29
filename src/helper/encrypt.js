c304c3408d18c9658874dc1552f20f1c73d80211
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2023-01-29 12:28:10 +0700

    fix some code

---
+++ src/helper/encrypt.js
@@ -0,0 +1,60 @@
+// const CryptoJS = require('crypto-js')
+import CryptoJS from 'crypto-js'
+
+const keySize = 256
+const iterations = 100
+
+const password = 'df287dfc1406ed2b692e1c2c783bb5cec97eac53151ee1d9810397aa0afa0d89'
+
+function encrypt (msg, pass) {
+  const salt = CryptoJS.lib.WordArray.random(128 / 8)
+
+  const key = CryptoJS.PBKDF2(pass, salt, {
+    keySize: keySize / 32,
+    iterations
+  })
+
+  const iv = CryptoJS.lib.WordArray.random(128 / 8)
+
+  const encrypted = CryptoJS.AES.encrypt(msg, key, {
+    iv,
+    padding: CryptoJS.pad.Pkcs7,
+    mode: CryptoJS.mode.CBC
+
+  })
+
+  // salt, iv will be hex 32 in length
+  // append them to the ciphertext for use  in decryption
+  const transitmessage = salt.toString() + iv.toString() + encrypted.toString()
+  return transitmessage
+}
+
+function decrypt (transitmessage, pass) {
+  const salt = CryptoJS.enc.Hex.parse(transitmessage.substr(0, 32))
+  const iv = CryptoJS.enc.Hex.parse(transitmessage.substr(32, 32))
+  const encrypted = transitmessage.substring(64)
+
+  const key = CryptoJS.PBKDF2(pass, salt, {
+    keySize: keySize / 32,
+    iterations
+  })
+
+  const decrypted = CryptoJS.AES.decrypt(encrypted, key, {
+    iv,
+    padding: CryptoJS.pad.Pkcs7,
+    mode: CryptoJS.mode.CBC
+
+  })
+
+  const decryptedResult = decrypted.toString(CryptoJS.enc.Utf8)
+
+  return decryptedResult
+}
+
+// const encrypted = encrypt(message, password)
+// const decrypted = decrypt(encrypted, password)
+
+// console.log(encrypted)
+// console.log(decrypted.toString(CryptoJS.enc.Utf8))
+
+export { encrypt, decrypt, password }
